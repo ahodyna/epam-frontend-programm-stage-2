39aa4987420c89c0a6b2ae7ce7b2c00aa967074e
alina <hodyna.alina@gmail.com>
2021-05-24 14:48:13 +0300

    homework 11: implemented task 2

--- FL16_HW11/homework/js/pipe.js
+++ FL16_HW11/homework/js/pipe.js
@@ -1,23 +1,27 @@
-// function isFunction(functionToCheck) {
-// 	return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-// }
+function isFunction(functionToCheck) {
+	return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
 
 const pipe = (value, ...funcs) => {
-	// PIPE implementation
+	try {
+		return funcs.reduce((previousVal, argument) => argument(previousVal), value)
+	} catch (err) {
+		return err;
+	}
 };
 
-// const replaceUnderscoreWithSpace = (value) => value.replace(/_/g, ' ');
-// const capitalize = (value) =>
-// 	value
-// 		.split(' ')
-// 		.map((val) => val.charAt(0).toUpperCase() + val.slice(1))
-// 		.join(' ');
-// const appendGreeting = (value) => `Hello, ${value}!`;
+const replaceUnderscoreWithSpace = (value) => value.replace(/_/g, ' ');
+const capitalize = (value) =>
+	value
+		.split(' ')
+		.map((val) => val.charAt(0).toUpperCase() + val.slice(1))
+		.join(' ');
+const appendGreeting = (value) => `Hello, ${value}!`;
 
-// const error = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, '');
+const error = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, '');
 
-// alert(error); // Provided argument at position 2 is not a function!
+alert(error); // Provided argument at position 2 is not a function!
 
-// const result = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, appendGreeting);
+const result = pipe('john_doe', replaceUnderscoreWithSpace, capitalize, appendGreeting);
 
-// alert(result); // Hello, John Doe!
+alert(result); // Hello, John Doe!
