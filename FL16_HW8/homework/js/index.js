320d0c3def23b3d6a7f85de7ab505fb2b47f0522
alina <hodyna.alina@gmail.com>
2021-07-13 18:15:04 +0300

    homework 8: JQuery; calculator

---
+++ FL16_HW8/homework/js/index.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+    const hundred = 100;
+    $('.calculator-button').click(function () {
+
+        if ($('#inputField').val() === '0') {
+            $('#inputField').val($(this).text())
+        } else {
+            $('#inputField').val($('#inputField').val() + $(this).text());
+        }
+    });
+
+    $('#equalsButton').click(function () {
+        const expresionString = $('#inputField').val()
+
+
+        if (isSafe(expresionString)) {
+            const calculationResult = count(expresionString)
+            let result = Math.round(calculationResult * hundred) / hundred
+            if (result === Infinity) {
+                $('#inputField').val('ERROR').css('color', 'red')
+            } else {
+                $('#inputField').val(result)
+                $(function () {
+                    if (/[4][8]{1,}/.test(result) || /[48]{2}/.test(expresionString)) {
+
+                        $('.text-log').prepend(`<div class='history-log'><div class='circle'>
+                        </div><div class='expression text-decoration'>${expresionString} = ${result}
+                        </div><div class='delete'>X</div></div>`);
+                    } else {
+                        $('.text-log').prepend(`<div class='history-log'><div class='circle'>
+                        </div><div class='expression'>${expresionString} = ${result}
+                        </div><div class='delete'>X</div></div>`);
+                    }
+                })
+            }
+        } else {
+            $('#inputField').val('ERROR')
+        }
+    })
+
+    $('.clean').click(function () {
+        $('#inputField').val('0')
+    })
+
+
+    $(document).on('click', '.circle', function () {
+        $(this).toggleClass('red-circle')
+    });
+
+
+    $(document).on('click', '.delete', function () {
+
+        $(this).closest('.history-log').remove();
+    });
+
+    $('.history-wrap').scroll(function () {
+        console.log(`Scroll Top: ${$(this).scrollTop()}`);
+    });
+
+    function isSafe(expression) {
+        return /(\d+\s*(\*|\/|\+|-)\s*)+(\d+\s*)/.test(expression)
+
+    }
+
+    function count(fn) {
+        return new Function('return ' + fn)();
+    }
+
+
+})
